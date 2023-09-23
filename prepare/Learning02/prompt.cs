ae7dc3bd49ee3c85769737cd7be835e586fa1261
tmd1248 <38797358+tmd1248@users.noreply.github.com>
2023-09-23 12:25:00 -0700

    journal for real this time

---
+++ prepare/Learning02/prompt.cs
@@ -0,0 +1,4 @@
+public class prompt {
+    public String[] _prompts = {"What was the strongest emotion I felt today?", "If I had one thing I could do over today, what would it be?", "Is there something I am looking forward to tomorrow?", "Is there someone I can reach out to today?" "Did I learn something new today?"}
+
+}   
